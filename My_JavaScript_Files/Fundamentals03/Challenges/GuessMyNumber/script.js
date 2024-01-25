eb9fbeee11cdc2d705aeb09ac67877749094655c
AamirShaikh966 <aamirshaikh5473@gmail.com>
2024-01-25 17:41:44 +0530

    Box-Model Done

--- My_JavaScript_Files/Fundamentals03/Challenges/GuessMyNumber/script.js
+++ My_JavaScript_Files/Fundamentals03/Challenges/GuessMyNumber/script.js
@@ -14,12 +14,17 @@
 let score = 20;
 let highScore = 0;
 let secretNumber = Math.round(Math.random() * 20) + 1;
+const btnCloseModal = document.querySelector(".close-modal");
+const overlay = document.querySelector(".overlay");
+const modal = document.querySelector(".modal");
+
+//To disabled the button
+// document.querySelector(".check").disabled = true;
 
 //to display message
 const displayMessage = function (message) {
   document.querySelector(".message").textContent = message;
 };
-
 //Guess the number
 document.querySelector(".check").addEventListener("click", function () {
   const guess = Number(document.querySelector(".guess").value);
@@ -28,11 +33,39 @@ document.querySelector(".check").addEventListener("click", function () {
   if (!guess) {
     displayMessage("⛔ No Number!");
   }
+  //if guess number is in negative or larger than 20
+  else if (guess < 0 || guess > 20) {
+    displayMessage("Enter number between 0 to 20 ");
+  }
 
+  // // if guess is wrong
+  else if (guess !== secretNumber) {
+    if (score > 1) {
+      displayMessage(guess > secretNumber ? "📈Too High!" : "📉Too Low!");
+      score--;
+      document.querySelector(".score").textContent = score;
+    } else {
+      displayMessage("💥You Lost the Game");
+      document.querySelector(".score").textContent = 0;
+      document.querySelector(".check").disabled = true;
+    }
+  }
   // If Guess is correct
   else if (guess === secretNumber) {
-    document.querySelector(".number").textContent = secretNumber;
+    // <!-- -----------------Box-Model Starts------------------- -->
+    overlay.classList.remove("hidden");
+    modal.classList.remove("hidden");
+
+    document.querySelector(".congratsmsg").textContent =
+      "Your Number is : " + guess + "\n You guessed the right number";
+
+    btnCloseModal.addEventListener("click", closeModal);
+
+    overlay.addEventListener("click", closeModal);
+    // <!-- -----------------Box-Model Ends------------------- -->
+
     displayMessage("🎉 Correct Answer!");
+    document.querySelector(".number").textContent = secretNumber;
     document.querySelector("body").style.backgroundColor = "#60b347";
 
     //For the HighScore
@@ -41,25 +74,22 @@ document.querySelector(".check").addEventListener("click", function () {
       document.querySelector(".highscore").textContent = highScore;
     }
   }
-
-  // if guess is wrong
-  else if (guess !== secretNumber) {
-    if (score > 1) {
-      displayMessage(guess > secretNumber ? "📈Too High!" : "📉Too Low!");
-      score--;
-      document.querySelector(".score").textContent = score;
-    }
-
-    //if score is become zero
-    else {
-      displayMessage("💥You Lost the Game");
-      document.querySelector(".score").textContent = 0;
-    }
-  }
 });
 
-// To restart the game
-document.querySelector(".again").addEventListener("click", function () {
+//function to show content inside the Box-Model
+const showModel = function () {
+  overlay.classList.remove("hidden");
+  modal.classList.remove("hidden");
+};
+
+//function to hide content inside the Box-Model
+const closeModal = function () {
+  overlay.classList.add("hidden");
+  modal.classList.add("hidden");
+};
+
+// To restart the game Function
+const restartGame = function () {
   score = 20;
   secretNumber = Math.trunc(Math.random() * 20) + 1;
 
@@ -68,4 +98,5 @@ document.querySelector(".again").addEventListener("click", function () {
   document.querySelector(".number").textContent = "?";
   document.querySelector(".guess").value = "";
   document.querySelector("body").style.backgroundColor = "#222";
-});
+};
+document.querySelector(".again").addEventListener("click", restartGame);
---
+++ My_JavaScript_Files/Fundamentals03/Challenges/ModelWindow/script.js
@@ -0,0 +1,25 @@
+"use strict";
+
+const btnsOpenModal = document.querySelectorAll(".show-modal");
+const modal = document.querySelector(".modal");
+const btnCloseModal = document.querySelector(".close-modal");
+const overlay = document.querySelector(".overlay");
+
+//function to show content
+const showModel = function () {
+  overlay.classList.remove("hidden");
+  modal.classList.remove("hidden");
+};
+
+//funtion to hide content
+const closeModal = function () {
+  overlay.classList.add("hidden");
+  modal.classList.add("hidden");
+};
+
+for (let i = 0; i < btnsOpenModal.length; i++) {
+  btnsOpenModal[i].addEventListener("click", showModel);
+}
+
+btnCloseModal.addEventListener("click", closeModal);
+overlay.addEventListener("click", closeModal);
